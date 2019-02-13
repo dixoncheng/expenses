247fe7292e79c64d008b84706a239d9b5ba9911c
Dixon Cheng <dixon@macaroni.co.nz>
2019-02-13 23:29:42 +1300

    reports wip

--- screens/ReportsScreen.js
+++ screens/ReportsScreen.js
@@ -1,22 +1,113 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { 
+  View,
+  StyleSheet,
+  Text,
+  DatePickerIOS,
+  Button,
+  ActivityIndicator,
+  Modal
+} from 'react-native';
 
-export default class LinksScreen extends React.Component {
+import { MailComposer } from 'expo';
+
+import * as firebase from 'firebase';
+import moment from 'moment';
+
+export default class ReportsScreen extends React.Component {
+  state = {
+    dateFrom: new Date(),
+    dateTo: new Date(),
+    loading: false,
+    items: []
+  };
+
   static navigationOptions = {
     title: 'Reports',
   };
 
+  setDateFrom = (newDate) => {
+    this.setState({ dateFrom: newDate });
+  };
+
+  setDateTo = (newDate) => {
+    this.setState({ dateTo: newDate });
+  };
+
+  onGenerate = () => {
+    this.setState({ loading: true });
+
+    // retrieve all expenses for the chosen dates
+    firebase.database().ref('/').once('value').then((snapshot) => {
+
+      let arr = Object.keys(snapshot.val()).map((key) => { return {key: key, ...snapshot.val()[key]} });
+      // console.log(arr);
+      this.setState({ items: arr });
+
+      // generate csv
+      
+
+
+      this.setState({ loading: false });
+
+      // attach to email and send
+      // MailComposer.composeAsync({ 
+      //   recipients: [ 'dixontk@gmail.com' ],
+      //   subject: `Expense report: ${moment(this.state.dateFrom).format('MMMM D, YYYY')} - ${moment(this.state.dateTo).format('MMMM D, YYYY')}`,
+      //   body: '',
+      //   // attachments: []
+      // });
+      // alert('Report has been emailed');
+
+    });
+  }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links 
-        <ExpoLinksView />*/}
+      <View style={styles.container}>
+        <Text style={styles.label}>From</Text>
+        <DatePickerIOS
+          date={this.state.dateFrom}
+          onDateChange={this.setDateFrom}
+          mode="date"
+          style={{ 
+            borderBottomWidth: 1,
+            borderBottomColor: 'lightgrey' 
+          }}
+        />
 
-        <Text>Generate reports here</Text>
+        <Text style={styles.label}>To</Text>
+        <DatePickerIOS
+          date={this.state.dateTo}
+          onDateChange={this.setDateTo}
+          mode="date"
+          style={{ 
+            borderBottomWidth: 1,
+            borderBottomColor: 'lightgrey' 
+          }}
+        />
+        <Button title="Generate" onPress={this.onGenerate} />
 
-      </ScrollView>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.loading}
+          >
+          <View
+            style={[
+              StyleSheet.absoluteFill,
+              {
+                backgroundColor: 'rgba(0,0,0,0.4)',
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+            ]}>
+            <ActivityIndicator color="#fff" animating size="large" />
+          </View>
+        </Modal>
+
+
+      </View>
     );
   }
 }
@@ -24,7 +115,18 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
   },
+  row: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+    justifyContent: 'space-between',
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 18,
+    textAlign: 'center'
+  }
 });
