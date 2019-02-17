f44d0609dfae8602dc376aae4882e052a26520db
Dixon Cheng <dixon@macaroni.co.nz>
2019-02-17 15:54:19 +1300

    report generation debug, android email compose

--- screens/ReportsScreen.js
+++ screens/ReportsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { 
+  Platform,
   View,
   StyleSheet,
   Text,
@@ -7,10 +8,13 @@ import {
   Button,
   ActivityIndicator,
   Modal,
-  Share
+  Share,
+  DatePickerAndroid,
+  TouchableHighlight,
+  Alert
 } from 'react-native';
 
-import { FileSystem } from 'expo';
+import { FileSystem, MailComposer } from 'expo';
 
 import * as firebase from 'firebase';
 import moment from 'moment';
@@ -37,17 +41,25 @@ export default class ReportsScreen extends React.Component {
   };
 
   setDateTo = (newDate) => {
-    this.setState({ dateTo: newDate });
+    // set to last second on the day so the expenses from that day are included
+    newDate.setHours(23, 59, 59);
+    // console.log(newDate.getTime());
+    // console.log(moment(newDate).format('MMMM Do YYYY, h:mm:ss a'));
+    this.setState({ dateTo: newDate }); //add 11pm 59 min
   }
 
   onGenerate = async () => {
     this.setState({ showingModal: true, loading: true });
 
+    // console.log(this.state);
+    // console.log(`startAt: ${this.state.dateFrom.getTime()}`);
+    // console.log(`endAt: ${this.state.dateTo.getTime()}`);
+
     // retrieve all expenses for the chosen dates
     // let snapshot = await firebase.database().ref('/').once('value');
     let snapshot = await firebase.database().ref('/').orderByChild('date').startAt(this.state.dateFrom.getTime()).endAt(this.state.dateTo.getTime()).once('value');
 
-    if(snapshot) {
+    if(snapshot.val()) {
       let arr = Object.keys(snapshot.val()).map((key) => { return {key: key, ...snapshot.val()[key]} });
       // console.log(arr);
       // this.setState({ showingModal: false, loading: false });
@@ -61,10 +73,27 @@ export default class ReportsScreen extends React.Component {
       await FileSystem.writeAsStringAsync(filename, report, { encoding: FileSystem.EncodingTypes.Base64 });
 
       this.setState({ loading: false, filename });
-      this.onShare();
+      
+      if(Platform.OS === 'ios') {
+        this.onShare();
+
+      } else {
+        this.setState({ showingModal: false });
+
+        MailComposer.composeAsync({ 
+          recipients: [ 'dixontk@gmail.com' ],
+          subject: `Expense report ${moment(this.state.dateFrom).format('MMM YY')} - ${moment(this.state.dateTo).format('MMM YY')}`,
+          attachments: [ filename ]
+        });
+      }
+
     } else {
       this.setState({ showingModal: false, loading: false });
-      alert('No records found');
+
+      setTimeout(() => {
+        Alert.alert('No records found');
+      }, 500);
+
     }
     
   }
@@ -163,30 +192,80 @@ export default class ReportsScreen extends React.Component {
     }
   };
 
+  selectDateAndroid = async (which) => {
+    try {
+      const {action, year, month, day} = await DatePickerAndroid.open({
+        // Use `new Date()` for current date.
+        // May 25 2020. Month 0 is January.
+        date: new Date()
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        // Selected year, month (0-11), day
+        if(which === 'from') {
+          this.setDateFrom(new Date(year, month, day))  
+        } else {
+          this.setDateTo(new Date(year, month, day))  
+        }
+      }
+    } catch ({code, message}) {
+      console.warn('Cannot open date picker', message);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.label}>From</Text>
-        <DatePickerIOS
-          date={this.state.dateFrom}
-          onDateChange={this.setDateFrom}
-          mode="date"
-          style={{ 
-            borderBottomWidth: 1,
-            borderBottomColor: 'lightgrey' 
-          }}
-        />
 
-        <Text style={styles.label}>To</Text>
-        <DatePickerIOS
-          date={this.state.dateTo}
-          onDateChange={this.setDateTo}
-          mode="date"
-          style={{ 
-            borderBottomWidth: 1,
-            borderBottomColor: 'lightgrey' 
-          }}
-        />
+        {Platform.OS === 'ios' &&
+        <View>
+          <Text style={styles.label}>From</Text>
+          <DatePickerIOS
+            date={this.state.dateFrom}
+            onDateChange={this.setDateFrom}
+            mode="date"
+            style={{ 
+              borderBottomWidth: 1,
+              borderBottomColor: 'lightgrey' 
+            }}
+          />
+
+          <Text style={styles.label}>To</Text>
+          <DatePickerIOS
+            date={this.state.dateTo}
+            onDateChange={this.setDateTo}
+            mode="date"
+            style={{ 
+              borderBottomWidth: 1,
+              borderBottomColor: 'lightgrey' 
+            }}
+          />
+        </View>
+        }
+        
+        {Platform.OS !== 'ios' &&
+        <View>
+          <TouchableHighlight
+            onPress={() => this.selectDateAndroid('from')}
+            underlayColor="lightgrey"
+            >
+            <View style={styles.row}>
+              <Text style={styles.label}>Date from</Text>
+              <Text style={styles.label}>{moment(this.state.dateFrom).format('D-MMM-YY')}</Text>
+            </View>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            onPress={() => this.selectDateAndroid('to')}
+            underlayColor="lightgrey"
+            >
+            <View style={styles.row}>
+              <Text style={styles.label}>Date to</Text>
+              <Text style={styles.label}>{moment(this.state.dateTo).format('D-MMM-YY')}</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+        }
+
         <Button title="Generate" onPress={this.onGenerate} />
 
         <Modal
@@ -222,6 +301,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   row: {
+    // flex: 1,
+    width: '100%',
     flexDirection: 'row',
     padding: 10,
     borderBottomWidth: 1,
